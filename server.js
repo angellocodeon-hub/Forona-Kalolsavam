7e720f1cdc4365fab4eee90b44fa12cb54f8b2a1
angellocodeon-hub <angellocodeon@gmail.com>
2026-09-19 21:36:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(Server running, on port ${PORT});
-});
+
 
 /* =====================================================
    MIDDLEWARE
